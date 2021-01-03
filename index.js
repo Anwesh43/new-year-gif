fd29bc1170c928be53ba7579ac916c8ed28021f2
Anwesh43 <anweshthecool0@gmail.com>
2021-01-04 01:45:07 +0530

    adding DrawingUtil class

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const w = 500 
 const h = 500
 const fontSize = 40
-const backColor = "white"
+const textColor = "white"
+const backColor = "black"
 const delay = 20 
 const scGap = 0.02 
+const text = "2020"
 
 const {Canvas} = require('canvas')
 const GifEncoder = require('gifencoder')
@@ -31,5 +33,30 @@ class ScaleUtil {
 
     static divideScale(scale, i, n) {
         return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawRotatingText(context, text, i, x, scale) {
+        const sf = ScaleUtil.sinify(scale)
+        const gap = h / (2 * text.length)
+        context.font = `${fontSize}px sans-serif`
+        context.fillStyle = textColor 
+        for (var j = 0; j < text.length; j++) {
+            const tw = context.measureText(text[i]).width
+            const sfi = ScaleUtil.divideScale(sf, 0, 2)
+            context.save()
+            context.translate(w / 2, gap * (i + 1))
+            context.rotate(2 * Math.PI * sfi)
+            context.scale(sfi, sfi)
+            context.fillText(text[i], -tw / 2, 0)
+            context.restore()
+        }
+    }
+
+    static drawBackground(context) {
+        context.fillStyle = backColor 
+        context.fillRect(0, 0, w, h)
     }
 }
