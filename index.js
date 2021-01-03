fe7e64a0b0ccc2098857a412a8260184d9d2790b
Anwesh43 <anweshthecool0@gmail.com>
2021-01-04 01:35:31 +0530

    adding State class

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const h = 500
 const fontSize = 40
 const backColor = "white"
 const delay = 20 
+const scGap = 0.02 
 
 const {Canvas} = require('canvas')
 const GifEncoder = require('gifencoder')
 
+class State {
 
+    constructor() {
+        this.scale = 0 
+    }
 
+    update(cb) {
+        this.scale += scGap 
+        if (Math.abs(this.scale) > 1) {
+            this.scale = 0 
+            cb()
+        }
+    }
+}
