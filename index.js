b34d2b1b452cd7ea78681fa3711faa2c80b54c90
Anwesh43 <anweshthecool0@gmail.com>
2021-01-04 01:32:59 +0530

    adding constants and libraries

---
+++ index.js
@@ -0,0 +1,11 @@
+const w = 500 
+const h = 500
+const fontSize = 40
+const backColor = "white"
+const delay = 20 
+
+const {Canvas} = require('canvas')
+const GifEncoder = require('gifencoder')
+
+
+
