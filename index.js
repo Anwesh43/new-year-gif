2eed15e00789d74c3df4f0bc3685396624eb66d2
Anwesh43 <anweshthecool0@gmail.com>
2021-01-04 01:51:39 +0530

    adding NewYearNode class

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const backColor = "black"
 const delay = 20 
 const scGap = 0.02 
 const text = "2020"
+const newText = "1"
 
 const {Canvas} = require('canvas')
 const GifEncoder = require('gifencoder')
@@ -38,19 +39,24 @@ class ScaleUtil {
 
 class DrawingUtil {
 
-    static drawRotatingText(context, text, i, x, scale) {
+    static drawRotatingText(context, scale) {
         const sf = ScaleUtil.sinify(scale)
+        const sfNext = DrawingUtil.divideScale(sf, 1, 2)
         const gap = h / (2 * text.length)
         context.font = `${fontSize}px sans-serif`
         context.fillStyle = textColor 
         for (var j = 0; j < text.length; j++) {
             const tw = context.measureText(text[i]).width
+            const tw1 = context.measureText(newText).width
             const sfi = ScaleUtil.divideScale(sf, 0, 2)
             context.save()
             context.translate(w / 2, gap * (i + 1))
             context.rotate(2 * Math.PI * sfi)
             context.scale(sfi, sfi)
-            context.fillText(text[i], -tw / 2, 0)
+            if (j == text.length - 1) {
+                context.fillText(text[i], -tw / 2 + (w * 0.5 + tw) * sfNext, 0)
+                context.fillText(newText, -tw1 / 2 - (w * 0.5 + tw) * (1 - sfNext), 0)
+            }
             context.restore()
         }
     }
@@ -58,5 +64,22 @@ class DrawingUtil {
     static drawBackground(context) {
         context.fillStyle = backColor 
         context.fillRect(0, 0, w, h)
+    }
+}
+
+class NewYearNode  {
+
+    constructor() {
+        this.state = new State()
+    }
+
+    draw(context, cb) {
+        DrawingUtil.drawBackground(context)
+        DrawingUtil.drawRotatingText(context, this.state.scale)
+        cb(context)
+    }
+
+    update(cb) {
+        this.state.update(cb)
     }
 }
